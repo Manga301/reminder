89b36005018c2f505c4bb937c59fefc10479fdc3
Mangaliso <mangaliso.twala@gmail.com>
2023-07-07 14:13:00 +0200

    add reminder

---
+++ index.js
@@ -0,0 +1,80 @@
+const reminderForm = document.getElementById("reminder-form");
+const reminderInput = document.getElementById("reminder-input");
+const inputMessage = document.getElementById("message");
+const reminderList = document.getElementById("reminder-list");
+const filterReminders = document.getElementById("filter");
+const clearBtn = document.getElementById("clear");
+
+// add a new reminder
+function onAddSubmit(e){
+    e.preventDefault();
+
+    let newReminder = reminderInput.value
+
+    // check if there's anything in the input
+    if(newReminder === ""){
+        inputMessage.classList.remove("hidden");
+        inputMessage.textContent = "Please add a reminder";
+        return;
+    }
+
+    // add reminder to DOM
+    addReminder(newReminder);
+
+    // clear input
+    reminderInput.value = "";
+
+    checkUI();
+}
+
+// add reminder element to DOM
+function addReminder(item){
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(item));
+
+    const button = createButton("btn-link red-text");
+    li.appendChild(button);
+
+    reminderList.appendChild(li);
+}
+
+// reminder button with icon
+function createButton(classes){
+    const button = document.createElement("button");
+    button.className = classes;
+    const icon = createIcon("fa-solid fa-xmark");
+    button.appendChild(icon);
+
+    return button;
+}
+
+// reminder element button icon
+function createIcon(classes){
+    const icon = document.createElement("i")
+    icon.className = classes;
+
+    return icon;
+}
+
+// check UI status
+function checkUI(){
+    const reminders = reminderList.querySelectorAll("li");
+
+    if(reminders.length === 0){
+        clearBtn.style.display = "none";
+        filterReminders.style.display = "none";
+    } else {
+        clearBtn.style.display = "block";
+        filterReminders.style.display = "block";
+    }
+
+}
+
+checkUI();
+
+// initialize app
+function init(){
+    reminderForm.addEventListener("submit", onAddSubmit);
+}
+
+init();
